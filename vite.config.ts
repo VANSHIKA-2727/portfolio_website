d97a9c1eba17c4010419726fa7b9467b7a535639
VANSHIKA-2727 <vanshikadhawan38@gmail.com>
2025-09-20 10:09:39 +0530

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const plugins = [react()];
+  
+  
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+      minify: "terser",
+    },
+  };
 });
